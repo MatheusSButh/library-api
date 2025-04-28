ded37dcb94e1b9984214f003a763a18b9f055278
Matheus <matheusdsouzabuth@gmail.com>
2025-04-28 16:16:04 -0300

    first commit

---
+++ src/test/java/com/buthdev/demo/LibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buthdev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
